498cf105070e27b169071cf76dbd6fae2f4f0b4a
Masahiko Sawada <sawada.mshk@gmail.com>
2017-11-30 20:37:05 +0900

    Complete query tuning.

--- shard-data-out-list.sql
+++ shard-data-out-list.sql
@@ -1,23 +1,20 @@
+select split_part(version(), ' ', 2) = '11devel' as pg11; \gset
+
 analyze;
+\if :pg11
 set enable_partition_wise_join to on;
+set enable_partition_wise_agg to on;
+\endif
+
 alter server "shard1" options (add use_remote_estimate 'on');
 alter server "shard2" options (add use_remote_estimate 'on');
 alter server "shard3" options (add use_remote_estimate 'on');
 alter server "shard4" options (add use_remote_estimate 'on');
 
---alter server "shard1" options (set fdw_tuple_cost '0.00000001');
---alter server "shard2" options (set fdw_tuple_cost '0.00000001');
---alter server "shard3" options (set fdw_tuple_cost '0.00000001');
---alter server "shard4" options (set fdw_tuple_cost '0.00000001');
+\if :pg11
+set enable_mergejoin to off;
+set enable_hashjoin to off;
+set enable_nestloop to off;
+\endif
 
---alter server "shard1" options (set fdw_startup_cost '100');
---alter server "shard2" options (set fdw_startup_cost '100');
---alter server "shard3" options (set fdw_startup_cost '100');
---alter server "shard4" options (set fdw_startup_cost '100');
-
---set enable_mergejoin to off;
---set enable_hashjoin to off;
---set enable_nestloop to off;
-
-
-explain analyze verbose select to_continent, count(*) from flight_bookings f left join hotel_bookings h on (f.to_continent = h.continent) group by to_continent;
+explain analyze verbose select to_continent, count(*) from flight_bookings f left join hotel_bookings h on (f.to_continent = h.continent) where h.booked_at > '2017-10-01' group by to_continent;
